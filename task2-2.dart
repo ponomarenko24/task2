a3a91e5a06a0be4a923c6b11b261147bfc317d11
Volodymyr Ponomarenko <vladimir.ponomarenko0924@gmail.com>
2025-03-06 19:04:32 +0200

    Updated check for correct input in task2-2

--- task2-2.dart
+++ task2-2.dart
@@ -7,14 +7,17 @@ void main() {
     while (true) {
       print("Input your number:");
       userInput = int.tryParse(stdin.readLineSync() ?? '') ?? 0;
-      if (userInput == generatedNumber) {
-        print("YOU WIN!!!");
-        break;
-      } else if (userInput > generatedNumber) {
-        print("Your number is bigger. Try again");
-      } else if (userInput < generatedNumber) {
-        print("Your number is lower. Try again");
+      if (userInput > 1 && userInput < 101) {
+        if (userInput == generatedNumber) {
+         print("YOU WIN!!!");
+         break;
+        } else if (userInput > generatedNumber) {
+         print("Your number is bigger. Try again");
+        } else if (userInput < generatedNumber) {
+         print("Your number is lower. Try again");
+        }
+      } else {
+        print("Wrong input. For correct game input integer number from 1 to 100");
       }
     }
-      
 }
